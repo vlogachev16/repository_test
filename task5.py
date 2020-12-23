39d9984b14dcc815cf15f04cee1feeeb79c21a55
vlogachev16 <viacheslavlogachev@gmail.com>
2020-12-23 13:09:27 +0100

    fifth task

---
+++ task5.py
@@ -0,0 +1,13 @@
+#Triangle task
+
+import math
+from math import sqrt
+
+a = 3
+b = 4
+
+c = sqrt(a ** 2 + b ** 2) # gipotinuza
+print(int(c))# result is in Float, but not int, WHY?
+
+d = (a * b) / 2 #square
+print(d)
