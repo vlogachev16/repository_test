e92789f4bce7256f8c4efab2e3d6044baf39343a
vlogachev16 <viacheslavlogachev@gmail.com>
2020-12-23 12:48:32 +0100

    first task

---
+++ task1.py
@@ -0,0 +1,15 @@
+"""addition, subtraction and multiplication of real numbers"""
+
+"""when I want to leave a comment in russian - receive an error 'Non-ASCII character' """
+
+a = 10
+b = 5
+
+c = a + b
+print(c)
+
+d = a - b
+print(d)
+
+e = a * b
+print(e)
