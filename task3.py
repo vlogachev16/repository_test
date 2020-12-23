cc714961e0083b997fca07246f59946fda47c65e
vlogachev16 <viacheslavlogachev@gmail.com>
2020-12-23 13:06:01 +0100

    third task

---
+++ task3.py
@@ -0,0 +1,10 @@
+# task with a cube
+
+a = 2 #rebro kuba
+b = 2 ** 3 # ob'em kuba
+
+print(b)
+
+c = a ** 2 #Ploshad' bokovoy poverchnosti kuba
+
+print(c)
---
+++ task4.py
@@ -0,0 +1,16 @@
+import math
+from math import sqrt
+#print(math.sqrt(4))
+
+a = 4
+b = 9
+
+c = (a + b) / 2
+print(c)
+
+#d = (a * b) ** 0.5
+#print(d)
+
+e = a * b
+f = sqrt(e)
+print(int(f))
